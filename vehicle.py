024bcf00e3f846be9453c08f4e17adaf20bd8919
seeni7 <Seeni.rengasamy@gmail.com>
2021-01-22 12:55:13 -0500

    Inheritance

--- vehicle.py
+++ vehicle.py
@@ -4,14 +4,22 @@ class Vehicle:
     Vehicle is a type that describes a machine that helps us travel.
 
     """
+  #  default_tire = 'tire'
 
-    def __init__(self, engine, tires):
+    def __init__(self, distance_traveled=0, unit='miles'):
 
-       self.engine = engine
-       self.tires = tires 
+       self.distance_traveled = distance_traveled
+       self.unit = unit 
+
+    # @classmethod
+    # def bycycle(cls, tires=None):
+    #     if not tires:
+    #         tires = [cls.default_tire, cls.default_tire]
+    #     return cls(None, tires)
        
 
     def description(self):
-        print(f"A vehicle with an {self.engine} engine and {self.tires} tires")
+       # print(f"A {self.__class__.__name__} that has traveled {self.distance_traveled} {self.unit}")
+       return f"A {self.__class__.__name__} that has traveled {self.distance_traveled} {self.unit}"
         
         
